e1d41ba9ab7f8e4fb19659749fea0c395f3ef49d
Yashaswini C <115557172+Yashaswini2004@users.noreply.github.com>
2025-07-29 11:56:48 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import "./styles.css";
+import AuthInputs from "./components/AuthInputs";
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <Header />
+      <main>
+        <AuthInputs />
+      </main>
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import "./index.css";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
